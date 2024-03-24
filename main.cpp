2a33a256288c5fe0b4ab277712a3be41d68b503e
Vedant Singh <vedantsinggh@gmail.com>
2024-03-24 20:13:38 +0530

    write hash to gnitconfig file

--- main.cpp
+++ main.cpp
@@ -8,21 +8,20 @@
 #include "log.h"
 #include "files.h"
 
-
 std::vector<std::string> all_dirs;
 std::vector<std::string> all_files;
 
-void scan_dir(std::string name){
+void scan_dir(std::string name, std::ofstream* config_file){
 	for (const auto & entry : std::filesystem::directory_iterator(name)){
 		if (entry.path().string() == "./.git") continue;
-		std::cout << entry.path();
 		if(std::filesystem::is_directory(entry.path())){
 			all_dirs.push_back(entry.path().string());	
-			scan_dir(entry.path().string());
-			std::cout << " is a folder!\n";
+			scan_dir(entry.path().string(), config_file);
 		}else{
 			all_files.push_back(entry.path().string());	
-			std::cout << " is a file!\n";
+			std::string hash = getFileHash(entry.path().string().c_str());
+			std::string res = entry.path().string() + ": " + hash + "\n";
+			*config_file << res;
 		}
 	}
 }
@@ -36,7 +35,7 @@ int main(int argc, char* argv[])
 		if(fileExists(config_file_path) != 0) {
 			std::ofstream file;
 			file.open(config_file_path);
-			file << ".gnitconfig";
+			file << ".gnitconfig\n";
 			file.close();
 			Log("config file initialized at current directory");
 			return 0;
@@ -59,7 +58,15 @@ int main(int argc, char* argv[])
 			Log(file);	
 		}
 
-		scan_dir("./");
+		const char*  config_file_path = ".gnitconfig";
+		if(fileExists(config_file_path) == 0) {
+			std::ofstream file;
+			file.open(config_file_path, std::ios::app);
+			scan_dir("./", &file);
+			file.close();
+		}else {
+			Log("NO GNIT CONFIG!");
+		}
 		
 		return 0;
 	});
