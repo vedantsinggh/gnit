2899170f0ac7ea6d0dae0919192f9371a6f8ab56
Vedant Singh <vedantsinggh@gmail.com>
2024-04-05 15:46:32 +0530

    improve scan_status

--- main.cpp
+++ main.cpp
@@ -12,9 +12,9 @@
 std::vector<std::string> all_dirs;
 std::vector<std::string> all_files;
 
-std::vector<std::string> old_file;
-std::vector<std::string> new_file;
-std::vector<std::string> updated_file;
+std::vector<std::string> old_files;
+std::vector<std::string> new_files;
+std::vector<std::string> updated_files;
 std::vector<std::string> deleted_files;
 std::vector<std::string> removed_files;
 
@@ -37,9 +37,9 @@ void add_scan(std::string name){
 				std::string new_hash = getFileHash(name.c_str());
 				if(old_hash != new_hash){
 					final_hash = new_hash;
-					updated_file.push_back(name);
+					updated_files.push_back(name);
 				}else {
-					old_file.push_back(name);
+					old_files.push_back(name);
 					final_hash = old_hash;
 				}
 				isFileFound = true;
@@ -58,7 +58,7 @@ void add_scan(std::string name){
 
 			std::string hash = getFileHash(name.c_str());
 			std::string res = name + ": " + hash + "\n";
-			new_file.push_back(name);
+			new_files.push_back(name);
 			config += res;	//add line to config
 
 		}
@@ -84,9 +84,9 @@ void add_scan(std::string name){
 						std::string new_hash = getFileHash(entry.path().string().c_str());
 						if(old_hash != new_hash){
 							final_hash = new_hash;
-							updated_file.push_back(entry.path().string());
+							updated_files.push_back(entry.path().string());
 						}else {
-							old_file.push_back(entry.path().string());
+							old_files.push_back(entry.path().string());
 							final_hash = old_hash;
 						}
 						isFileFound = true;
@@ -105,7 +105,7 @@ void add_scan(std::string name){
 
 					std::string hash = getFileHash(entry.path().string().c_str());
 					std::string res = entry.path().string() + ": " + hash + "\n";
-					new_file.push_back(entry.path().string());
+					new_files.push_back(entry.path().string());
 					config += res;				
 
 				}
@@ -114,7 +114,7 @@ void add_scan(std::string name){
 	}
 }
 
-void status_scan(std::string name){
+void scan_files(std::string name){
 
 	if(fileExists(config_file_path) != 0) {
 		Log(ERROR, "NO CONFIG FILE!");
@@ -126,37 +126,75 @@ void status_scan(std::string name){
 			if (entry.path().string() == config_file_path) continue;
 
 			if(std::filesystem::is_directory(entry.path())){
-				status_scan(entry.path().string());
+				all_dirs.push_back(entry.path().string());
+				scan_files(entry.path().string());
 			}else{
+				all_files.push_back(entry.path().string());
+			}
+		}
+	}
+}
 
-				std::string line;
-				std::ifstream config_file(config_file_path);
-				bool isFileFound = false;
+void scan_status(){
+	if(fileExists(config_file_path) != 0) {
+		Log(ERROR, "NO CONFIG FILE!");
+	}
+	else{
+		
+		std::string line;
+		std::ifstream config_file(config_file_path);
 
-				while(getline(config_file, line))
-				{
-					if(line.rfind(entry.path().string(), 0 ) == 0){
-
+		while(getline(config_file, line))
+		{
+			int pos = line.find(":");
+			std::string name  = line.substr(0 , pos);
+			
+			if(fileExists(name.c_str()) != 0){
+				removed_files.push_back(name);
+			}else{
+				for (std::string item : all_files){
+					if(item == name){
 						std::string old_hash = line.substr( line.size() - 64, line.size());
-						std::string new_hash = getFileHash(entry.path().string().c_str());
-
+						std::string new_hash = getFileHash(item.c_str());
+		
 						if(old_hash != new_hash){
-							updated_file.push_back(entry.path().string());
+							updated_files.push_back(item);
 						}else {
-							old_file.push_back(entry.path().string());
+							old_files.push_back(item);
 						}
-
-						isFileFound = true;
 						break;
 					}
 				}
-				
-				config_file.close();
-
-				if(!isFileFound){
-					new_file.push_back(entry.path().string());
-				}
 			}
+		}
+		config_file.close();
+	}
+}
+
+void scan_new_files(){
+	std::vector<std::string> temp_f;
+	std::string line;
+	std::ifstream config_file(config_file_path);
+
+	while(getline(config_file, line))
+	{
+		int pos = line.find(":");
+		std::string name  = line.substr(0 , pos);
+		temp_f.push_back(name);
+	}
+	config_file.close();
+
+	for(std::string item : all_files){
+		bool found = false;
+		for(std::string t : temp_f){
+			if(t == item)
+			{
+				found = true;
+				break;
+			}
+		}
+		if(!found){
+			new_files.push_back(item);
 		}
 	}
 }
@@ -172,7 +210,7 @@ int main(int argc, char* argv[])
 		if(fileExists(config_file_path) != 0) {
 			std::ofstream file;
 			file.open(config_file_path);
-			file << ".gnitconfig\n";
+			file << "\n";
 			file.close();
 			Log("config file initialized at current directory");
 			return 0;
@@ -183,42 +221,28 @@ int main(int argc, char* argv[])
 
 	
 	add_argument(&cmd,"status" , [&](int arc, char* arv[]) -> int{
-		
-		std::string line;
-		std::ifstream config_file(config_file_path);
 
-		while(getline(config_file, line))
-		{
-			int pos = line.find(":");
-			
-			std::string name  = line.substr(0 , pos);
-			if(fileExists(name.c_str()) != 0){
-				removed_files.push_back(name);
-			}
-		}
-		
-		config_file.close();
+		scan_files("./");	
+		scan_status();		
+		scan_new_files();
 
-		status_scan("./");		
-
-		if (new_file.size() == 0){
+		if (new_files.size() == 0){
 			Log(INFO, "NO NEW FILE!");
 		}else{
 			Log(GREEN, "NEW FILES:");
-			for(std::string f : new_file){
+			for(std::string f : new_files){
 				Log(GREEN," + " + f);
 			}
 		}
 		
-		if (updated_file.size() == 0){
+		if (updated_files.size() == 0){
 			Log(INFO, "NO FILE MODIFIED!");
 		}else{
 			Log(VOILET, "MODIFIED FILES:");
-			for(std::string f : updated_file){
+			for(std::string f : updated_files){
 				Log(VOILET," ~ " + f);
 			}
 		}
-
 		
 		if (removed_files.size() == 0){
 			Log(INFO, "NO DELETED FILE!");
